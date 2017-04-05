e7cf98c01ad62aed7212b08858c93f338d6dabfe
Sergiy <7s.gorbatyuk@gmail.com>
2017-04-05 15:01:53 +0300

    у

--- portfolio/gorbatyuk/application.php
+++ portfolio/gorbatyuk/application.php
@@ -3,16 +3,16 @@
 <head>
 <meta charset="UTF-8">
 <title>Создание формы обратной связи</title>
-<meta http-equiv="Refresh" content="4; URL=http://index.html/"> 
+<meta http-equiv="Refresh" content="4; URL=https://github.com/gorbatyuks/gorbatyuks.github.io/blob/master/portfolio/gorbatyuk/contact.html">
 </head>
 <body>
 
 <?php 
 
-$sendto   = "smart-landing@yandex.ru"; // почта, на которую будет приходить письмо
+$sendto   = "7s.gorbatyuk@gmail.com";
 $username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
 $usertel = $_POST['telephone']; // сохраняем в переменную данные полученные из поля c телефонным номером
-$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
+$usermail = $_POST['time']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
 
 // Формирование заголовка письма
 $subject  = "Новое сообщение";
@@ -25,18 +25,19 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
 $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>\r\n";
 $msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
-$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Время:</strong> ".$usermail."</p>\r\n";
 $msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
 $msg .= "</body></html>";
 
 // отправка сообщения
 if(@mail($sendto, $subject, $msg, $headers)) {
-	echo "<center><img src='images/spasibo.png'></center>";
+	echo "<center><img src='image/spasibo.png'></center>";
 } else {
-	echo "<center><img src='images/ne-otpravleno.png'></center>";
+	echo "<center><img src='image/ne-otpravleno.png'></center>";
 }
 
 ?>
+
 
 </body>
 </html>
